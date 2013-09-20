72c6469a9d104663e8769215076e4dbd351fdd03
codesliced <codesliced@gmail.com>
2013-09-20 10:32:45 -0700

    Exercise #3

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-/* JS goes here */
+$(document).ready(function () {
+
+    $('li').click(function (e) {
+        if ($(this).text() == "Sidebar") {
+            setTimeout(function () {
+                $(".sidebar").show();
+            }, 3000);
+        }
+    });
+
+    $("#blue").hover(function () {
+        $('a', this).css("color", "green");
+        $("a", ".link").css("color", "blue");
+    },
+
+    function () {
+        $("a", ".link").css("color", "green");
+    });
+});
